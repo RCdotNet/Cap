104cad568e2c3e85dcbbf0d0f852dd15f4071db4
Benson Gikonyo <muhiugikonyo@gmail.com>
2020-09-16 22:47:04 +0300

    updated eksctl-create-cluster.sh

--- scripts/eksctl-create-cluster.sh
+++
@@ -1,9 +0,0 @@
-eksctl create cluster \
---name capstonecluster \
---version 1.17 \
---region us-east-2b \
---nodegroup-name linux-nodes \
---nodes 2 \
---nodes-min 2 \
---nodes-max 2 \
---node-type t3.medium \--managed 
