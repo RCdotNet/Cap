92c20c4cf165f997ed70dd478539f0f7b03ab2e5
Benson Gikonyo <muhiugikonyo@gmail.com>
2020-09-17 16:03:22 +0300

    updated jenkins file added grand deployment script

--- Jenkinsfile
+++ Jenkinsfile
@@ -40,6 +40,7 @@ pipeline {
               steps{
                   echo 'Deploying to AWS...'
                   withAWS(credentials: 'newec2user', region: 'us-east-2') {
+                      sh 'aws sts get-caller-identity'
                       sh "aws eks --region us-east-2 update-kubeconfig --name capstonecluster"
                       sh "kubectl apply -f deployment/deployment.yml"
                       sh "kubectl get nodes"
---
+++ grand-deployment.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+./create-stack.sh cluster file-cluster.json parameters.json
+aws cloudformation wait stack-create-complete --stack-name cluster
+
+./create.sh nodes file-node.json parameters.json
+aws cloudformation wait stack-create-complete --stack-name nodes
+
+aws eks update-kubeconfig --name capstonecluster
+
+kubectl apply -f deployment/deploymet.yml
+kubectl apply -f deployment/load-balancer.yml
+
+kubectl rollout status deployment/projectcapstone
+kubectl get service
