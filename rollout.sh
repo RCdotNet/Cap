3628a83499bd1d4b0b952ee768af7273c9121e35
Ubuntu <ubuntu@ip-172-31-4-215.us-west-2.compute.internal>
2020-09-19 09:14:08 +0000

    show kubecl config all contexts

--- rollout.sh
+++ rollout.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 kubectl config current-context
+kubectl config get-contexts
 kubectl apply -f deployment/deployment.yml
 #kubectl get deployment capstone-$target -o=yaml | sed -e "s/$target/$appver/g" | kubectl apply -f - 
 kubectl rollout status deployment/projectcapstone
